b1652c8ef1876ab6f30163286d80be425759a382
bodya18 <bodya18x@mail.ru>
2021-08-10 11:20:03 +0300

    a

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res)=>{
     var zip = new AdmZip(`${__dirname}/Прайс LBS 27.07.xlsx`);
     zip.extractEntryTo("xl/media/", `${__dirname}/media`, /*maintainEntryPath*/false, /*overwrite*/true);
     res.end('hi')
-})
+}) 
 
 app.get('/a', (req, res)=>{
 
