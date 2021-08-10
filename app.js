30985d9de350d6e14f541c715e474bd3373ee0f4
bodya18 <bodya18x@mail.ru>
2021-08-10 10:57:12 +0300

    creating database, adding model, parsing data with xlsx

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+var xlsx = require('node-xlsx').default;
+var AdmZip = require('adm-zip');
+require('./model/tables');
+
+app.get('/', (req, res)=>{
+    const workSheetsFromFile = xlsx.parse(`${__dirname}/Прайс LBS 27.07.xlsx`);
+    console.log(workSheetsFromFile[0].data); //length 9
+    var zip = new AdmZip(`${__dirname}/Прайс LBS 27.07.xlsx`);
+    zip.extractEntryTo("xl/media/", `${__dirname}/media`, /*maintainEntryPath*/false, /*overwrite*/true);
+    res.end('hi')
+})
+
+app.get('/a', (req, res)=>{
+
+    res.end('12321')
+})
+app.listen(3000)
---
+++ model/tables.js
@@ -0,0 +1,76 @@
+const Sequelize = require("sequelize");
+ 
+const sequelize = new Sequelize("shop", "root", "ZAQwsxz1.", {
+    dialect: "mysql",
+    host: "localhost",
+    define: {
+      timestamps: false
+    }
+    // ,logging: false
+});
+
+const Product = sequelize.define("product", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    article: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+});
+
+const DimensionProduct = sequelize.define("DimensionProduct", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    dimension: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    count: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    price: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
+});
+Product.hasMany(DimensionProduct);
+
+
+const _order = sequelize.define("_order", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    number: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+});
+DimensionProduct.hasMany(_order);
+
+module.exports = sequelize.sync({force:true}).then(()=>{
+    console.log("Tables have been created");
+}).catch(err=>console.error(err));
