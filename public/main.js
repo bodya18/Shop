a1e2df9c900e52318f2c70f6a4980de641339f87
bodya18 <bodya18x@mail.ru>
2021-09-01 12:00:16 +0300

    output main_bottom text

--- public/main.js
+++ public/main.js
@@ -8,12 +8,15 @@ addEventListener('load', ()=>{
     request.addEventListener("load", function () {
         let data = JSON.parse(request.response)
         let main_quote = document.getElementById('main_quote')
-        
+        let Main_bottom_text = document.getElementById('Main_bottom_text')
         for (const i in data) {
             if (data[i]._key === 'Main_quote') {
                 console.log(data[i].value);
                 main_quote.insertAdjacentHTML('beforeend', data[i].value)
-                break;
+                continue;
+            }
+            if(data[i]._key === 'Main_bottom_text'){
+                Main_bottom_text.insertAdjacentHTML('beforeend', data[i].value)
             }
         }
     })
