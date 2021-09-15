82ca1e593f724f9fdd113d744c5d537ecb80d1be
bodya18 <bodya18x@mail.ru>
2021-09-15 13:47:58 +0300

    test in server

--- services/product.js
+++ services/product.js
@@ -11,9 +11,10 @@ class Product{
     }
 
     async ParseData(filedata){
+        console.log(filedata);
         if(!filedata || (filedata.mimetype != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || 
         filedata.mimetype != "application/vnd.ms-excel")){
-            console.log(!filedata || (filedata.mimetype != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || filedata.mimetype != "application/vnd.ms-excel"));
+            console.log(!filedata, (filedata.mimetype != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filedata.mimetype != "application/vnd.ms-excel"));
             return {perm: false, error: 'Файл должен быть разшерением .xlsx или .xls'}
         }
         const workSheetsFromFile = xlsx.parse(`${process.env.dirname}/${filedata.path}`);
